b231d62306ad17144a4ee7b31280422c673fb9f8
student <student@MININT-KRH39VC>
2015-06-22 20:12:57 -0700

    My commit during class.

---
+++ ITT-MASTER/src/com/itt/start/helloWorld.java
@@ -0,0 +1,11 @@
+package com.itt.start;
+
+public class helloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+
+	
+	}
+
+}
---
+++ ITT-MASTER/src/comm/itt/ifthenelse/IfThenElse.java
@@ -0,0 +1,15 @@
+package comm.itt.ifthenelse;
+
+public class IfThenElse {
+	static int x= 10;
+
+	public static void main(String[] args) {
+		if (x<20){
+			System.out.println("Yes x is less than 20");
+		} else{
+			System.out.println("No x is not less than 20");
+		}
+
+	}
+
+}
