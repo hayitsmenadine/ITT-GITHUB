0b219c62a6537132294798f81cf961a1f4a552af
student <student@MININT-KRH39VC>
2015-06-22 21:54:48 -0700

    June 22, 2015

---
+++ ITT-MASTER/src/comm/itt/constructorexample/BasicConstructorExample.java
@@ -0,0 +1,13 @@
+package comm.itt.constructorexample;
+
+public class BasicConstructorExample {
+
+	public static void main(String[] args) {
+		stuff stuff1= new stuff();
+		stuff stuff2= new stuff();
+		stuff stuff3= new stuff();
+		
+
+	}
+
+}
---
+++ ITT-MASTER/src/comm/itt/constructorexample/stuff.java
@@ -0,0 +1,9 @@
+package comm.itt.constructorexample;
+
+public class stuff {
+static int count = 0;
+public stuff(){
+	count=count +1;
+			System.out.println("Created object number = " +count);
+}
+}
